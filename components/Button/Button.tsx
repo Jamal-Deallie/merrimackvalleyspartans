44472afc003b9617caf8e6bd70cf5023f936c140
Jamal-Deallie <66758645+Jamal-Deallie@users.noreply.github.com>
2022-12-16 19:07:21 -0600

    working on button bug

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,40 +1,48 @@
-import {
-  ButtonHTMLAttributes,
-  ReactElement,
-  useCallback,
-  ReactNode,
-} from 'react';
-import { CustomButton } from './styles';
+// import {
+//   ButtonHTMLAttributes,
+//   ReactElement,
+//   useCallback,
+//   ReactNode,
+// } from 'react';
+// import { CustomButton } from './styles';
 
-export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  children: string | ReactNode;
-  onClick?: () => void;
-  icon?: ReactElement;
-  width?: string;
-  color?:
-    | 'primary'
-    | 'secondary'
-    | 'tertiary'
-    | 'transparent'
-    | 'outline_primary'
-    | 'outline_secondary';
-  isLoading?: boolean;
+// export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+//   children: string | ReactNode;
+//   onClick?: () => void;
+//   icon?: ReactElement;
+//   width?: string;
+//   color?:
+//     | 'primary'
+//     | 'secondary'
+//     | 'tertiary'
+//     | 'transparent'
+//     | 'outline_primary'
+//     | 'outline_secondary';
+//   isLoading?: boolean;
+// };
+
+// export default function Button({
+//   children,
+//   icon,
+//   isLoading = false,
+//   className,
+//   onClick,
+//   color,
+//   width,
+//   disabled = false,
+//   ...props
+// }: ButtonProps) {
+//   return (
+//     <CustomButton {...props} color={color} disabled={disabled}>
+//       {children}
+//     </CustomButton>
+//   );
+// }
+
+type Props = {};
+
+const Button = (props: Props) => {
+  return <div>Button</div>;
 };
 
-export default function Button({
-  children,
-  icon,
-  isLoading = false,
-  className,
-  onClick,
-  color,
-  width,
-  disabled = false,
-  ...props
-}: ButtonProps) {
-  return (
-    <CustomButton {...props} color={color} disabled={disabled}>
-      {children}
-    </CustomButton>
-  );
-}
+export default Button;
