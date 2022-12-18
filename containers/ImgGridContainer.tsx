3adf884e8bf3f2e253f3918f61d86a86d5816146
Jamal-Deallie <66758645+Jamal-Deallie@users.noreply.github.com>
2022-12-18 02:02:22 -0600

    added styles and switched to sass

--- containers/ImgGridContainer.tsx
+++ containers/ImgGridContainer.tsx
@@ -7,3 +7,4 @@ const ImgGridContainer = (props: Props) => {
     <div>ImgGridcContainer</div>
   )
 }
+// background-image: radial-gradient( circle 333px at 3.7% 53.6%,  rgba(255,144,44,1) 0%, rgba(252,76,76,1) 90% );
